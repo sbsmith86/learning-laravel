0b3b0be5c3e3965def2c7935bc31e09bdcd6ca28
Shae Smith <ssmith@dosomething.org>
2015-05-26 17:13:39 -0400

    form requests and controller validation

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,33 +2,50 @@
 
 use App\Article;
 use App\Http\Requests;
+use App\Http\Requests\CreateArticleRequest;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 
-use Request;
-
 class ArticlesController extends Controller {
 
+  /*
+   * Show all published articles.
+   *
+   * @return articles view
+   */
 	public function index() {
     $articles = Article::latest()->published()->get();
 
     return view('articles.index', compact('articles'));
   }
 
+  /*
+   * Show a specific articles with id
+   *
+   * @return article page view
+   */
   public function show($id) {
     $article = Article::findOrFail($id);
-
-    dd($article->published_at);
 
     return view('articles.show', compact('article'));
   }
 
+  /*
+   * Form to create an article.
+   *
+   * @return create form page
+   */
   public function create() {
     return view('articles.create');
   }
 
-  public function store() {
-    Article::create(Request::all());
+  /*
+   * Handle POST requests from the create form. Saves an article in the db.
+   *
+   * @return Redirect to article listings page.
+   */
+  public function store(CreateArticleRequest $request) {
+    Article::create($request->all());
 
     return redirect('articles');
 
