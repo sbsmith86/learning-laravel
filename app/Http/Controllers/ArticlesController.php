d7dd6e233456b5b0f9bf51c758ff1ede9d039a54
Shae Smith <ssmith@dosomething.org>
2015-05-27 11:40:37 -0400

    View partials and form reuse

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,7 +2,7 @@
 
 use App\Article;
 use App\Http\Requests;
-use App\Http\Requests\CreateArticleRequest;
+use App\Http\Requests\ArticleRequest;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 
@@ -44,11 +44,24 @@ class ArticlesController extends Controller {
    *
    * @return Redirect to article listings page.
    */
-  public function store(CreateArticleRequest $request) {
+  public function store(ArticleRequest $request) {
     Article::create($request->all());
 
     return redirect('articles');
 
   }
 
+  public function edit($id) {
+    $article = Article::findOrFail($id);
+
+    return view('articles.edit', compact('article'));
+  }
+
+  public function update($id, ArticleRequest $request) {
+    $article = Article::findOrFail($id);
+
+    $article->update($request->all());
+
+    return redirect('articles');
+  }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,6 @@
 <?php
-Route::get('/', 'WelcomeController@index');
 Route::get('about','PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
-Route::get('articles', 'ArticlesController@index');
-Route::get('articles/create', 'ArticlesController@create');
-Route::get('articles/{id}', 'ArticlesController@show');
-Route::post('articles', 'ArticlesController@store');
+
+Route::resource('articles', 'ArticlesController');
