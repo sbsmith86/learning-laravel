db96bea57c3170a0fd159ccc81f5ea6cac6452f1
Shae Smith <ssmith@dosomething.org>
2015-05-27 17:15:38 -0400

    starting on user auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,3 +4,8 @@ Route::get('contact', 'PagesController@contact');
 
 
 Route::resource('articles', 'ArticlesController');
+
+Route::controllers([
+  'auth' => 'Auth\AuthController',
+  'password' => 'Auth\PasswordController',
+]);
